d4728de1a9c0be5f40ea05ca822af9db084931db
Roomadu Wickrama <roomaduanjana@gmail.com>
2025-12-28 22:59:29 +0530

    Update PropertyCard.jsx

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,15 +1,22 @@
+
 import { Link } from "react-router-dom";
 import "./PropertyCard.css";
 
-function PropertyCard({ property }) {
-  return (
-    <div className="card">
-      <img src={property.picture} alt="property" />
-      <h3>£{property.price.toLocaleString()}</h3>
-      <p>{property.type} · {property.bedrooms} bedrooms</p>
-      <Link to={`/property/${property.id}`}>View Details</Link>
-    </div>
-  );
+function PropertyCard({ property, addFavourite }) {
+    return (
+        <div className="card">
+            <img src={property.picture} alt="" />
+            <h3>£{property.price.toLocaleString()}</h3>
+            <p>{property.type} · {property.bedrooms} beds</p>
+            <p>{property.location}</p>
+
+            <button onClick={() => addFavourite(property)}>
+                ❤ Favourite
+            </button>
+
+            <Link to={`/property/${property.id}`}>View Details</Link>
+        </div>
+    );
 }
 
 export default PropertyCard;
