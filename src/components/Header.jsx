0efa2aedc0eb62bbe1e558a66c7a7e4013c96241
Roomadu Wickrama <roomaduanjana@gmail.com>
2025-12-28 12:08:00 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="/logo.jpg" alt="Website Logo" />
+      <h2>Dream Properties</h2>
+    </header>
+  );
+}
+
+export default Header;
