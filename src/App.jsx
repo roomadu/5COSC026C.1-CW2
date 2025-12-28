a9d64ae022e8c2a0321d40e21d0db9743227836b
Roomadu Wickrama <roomaduanjana@gmail.com>
2025-12-28 12:07:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 import { useState } from 'react'
 import './App.css'
@@ -51,3 +52,19 @@ export default function App() {
     </div>
   )
 }
+=======
+import { Routes, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage.jsx";
+import PropertyPage from "./pages/PropertyPage.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<SearchPage />} />
+      <Route path="/property/:id" element={<PropertyPage />} />
+    </Routes>
+  );
+}
+
+export default App;
+>>>>>>> Stashed changes
