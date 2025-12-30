d1bd44d13c63be015df9ada1532c2350b9e3bb8d
Roomadu Wickrama <roomaduanjana@gmail.com>
2025-12-30 23:59:32 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,16 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <img src="/logo.jpg" alt="Website Logo" />
-      <h2>Dream Properties</h2>
+      <div className="header-left">
+        <img src="/logo.png" alt="Website Logo" />
+        <h2>Dream Properties</h2>
+      </div>
+
+      <nav className="header-nav">
+        {/* Scroll links to sections on SearchPage */}
+        <a href="/#search-section">Search Properties</a>
+        <a href="/#properties-section">Properties</a>
+      </nav>
     </header>
   );
 }
