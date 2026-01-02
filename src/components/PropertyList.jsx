a07e8f4d58f552dbb0b8ebca7d02362c203e72dd
Roomadu Wickrama <roomaduanjana@gmail.com>
2026-01-03 02:14:41 +0530

    Update PropertyList.jsx

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -4,12 +4,13 @@ import "./PropertyList.css";
 function PropertyList({ properties, addFavourite, favourites = [] }) {
   return (
     <div className="grid">
+      {/* Loop through each property and render a card */}
       {properties.map((p) => (
         <PropertyCard
           key={p.id}
           property={p}
-          addFavourite={addFavourite}
-          favourites={favourites} 
+          addFavourite={addFavourite} // For click or drag-to-favourites
+          favourites={favourites}     // To check if already favourited
         />
       ))}
     </div>
