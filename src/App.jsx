b747f425f464ee7ebff14c8d847a8d921ad02e03
Roomadu Wickrama <roomaduanjana@gmail.com>
2026-01-03 00:28:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import SearchPage from "./pages/SearchPage";
 import PropertyPage from "./pages/PropertyPage";
+import "./App.css";
+
+
 
 function App() {
+  const [favourites, setFavourites] = useState([]);
+
+  const addFavourite = (property) => {
+    setFavourites(prev =>
+      prev.some(p => p.id === property.id) ? prev : [...prev, property]
+    );
+  };
+
+  const removeFavourite = (id) => {
+    setFavourites(prev => prev.filter(p => p.id !== id));
+  };
+
+  const clearFavourites = () => setFavourites([]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/property/:id" element={<PropertyPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <SearchPage
+            favourites={favourites}
+            addFavourite={addFavourite}
+            removeFavourite={removeFavourite}
+            clearFavourites={clearFavourites}
+          />
+        }
+      />
+
+      <Route
+        path="/property/:id"
+        element={
+          <PropertyPage
+            favourites={favourites}
+            addFavourite={addFavourite}
+            removeFavourite={removeFavourite}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
